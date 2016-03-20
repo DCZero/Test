e86d2106fe844ec279d92e91adf553c1e9766901
DCZero <dividethesundayfromtheweek@gmail.com>
2016-03-20 09:30:24 +0000

    adding linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge');
-
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -13,23 +13,32 @@ const common = {
 	// entry accepts a path or an object of etnries. We'll be using
 	// the latter form given it's convenient with more complex
 	// configurations
-
-	entry: {
+	eslint: {
+		configFile: path.join(__dirname, '.eslintrc'),
+	},
+	entry: 	{
 		app: PATHS.app
 	},
 	output: {
 		path: PATHS.build,
-		filename: 'bundle.js'
+		filename: 'bundle.js' 
 	},
 	module: {
+        preLoaders: [
+            { 
+            	test: /\.jsx?$/, 
+            	loader: 'eslint', 
+            	include: PATHS.app
+            }
+        ],
 		loaders: [
-		{
-			test: /\.css$/,
-			loaders: ['style', 'css'],
-			include: PATHS.app
-		}
-		]
-	}
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.app 
+			}
+			]
+	}+
 };
 
 // Default
@@ -38,19 +47,22 @@ if(TARGET === 'start' || !TARGET) {
 		devServer: {
 			contentBase: PATHS.build,
 			// Enable history API fallback so HTML5 History API based
-      		// routing works. This is a good default that will come
-      		// in handy in more complicated setups.
+			// routing works. This is a good default that will come
+			// in handy in more complicated setups.
 			historyApiFallback: true,
 			hot: true,
 			inline: true,
 			progress: true,
-
+			devtool: 'eval-source-map',
 			stats: 'errors-only',
 			host: process.env.HOST,
 			port: process.env.PORT
 		},
 		plugins: [
-			new webpack.HotModuleReplacementPlugin()
+			new webpack.HotModuleReplacementPlugin(),
+			new NpmInstallPlugin({
+				save: true // --save
+			})
 		]
 	});
 }
